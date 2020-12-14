f144e38ebc929ad097908e79794b8022d65ee63f
Badar Munir <badar.munir@mazikglobal.com>
2020-12-14 16:56:41 +0500

    Changes

--- Build Sanity Suit/TestCases/Payers/Create_Payers.cs
+++ Build Sanity Suit/TestCases/Payers/Create_Payers.cs
@@ -1,11 +1,13 @@
 ﻿
+using AventStack.ExtentReports;
 using Microsoft.Dynamics365.UIAutomation.Api.UCI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using OpenQA.Selenium;
+using System.IO;
 
 namespace Build_Sanity_Suit
 {
-    //[TestClass]
+    [TestClass]
     public class A3_Create_Payers : TestBase
     {
         public string PayerNum;
@@ -32,6 +34,26 @@ namespace Build_Sanity_Suit
         [TestCleanup]
         public void Teardown()
         {
+            if (TestContext.CurrentTestOutcome.ToString() == "Passed")
+            {
+
+                test.Log(Status.Info, "Test Ended");
+                test.Log(Status.Pass, "Test Passed");
+                Screenshot ss = ((ITakesScreenshot)client.Browser.Driver).GetScreenshot();
+                string path = Directory.GetCurrentDirectory() + TestContext.TestName;
+                ss.SaveAsFile(path);
+                this.TestContext.AddResultFile(path);
+
+            }
+            else if (TestContext.CurrentTestOutcome.ToString() == "Failed")
+            {
+                test.Log(Status.Info, "Test Ended");
+                test.Log(Status.Fail, "Test Failed");
+                Screenshot ss = ((ITakesScreenshot)client.Browser.Driver).GetScreenshot();
+                string path = Directory.GetCurrentDirectory() + TestContext.TestName;
+                ss.SaveAsFile(path);
+                this.TestContext.AddResultFile(path);
+            }
             Cleanup("Payer Number:" + PayerNum + "\r\n");
             client.Browser.Driver.Close();
         }
